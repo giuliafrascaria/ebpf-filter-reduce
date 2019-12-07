73194f43cd9eb3039788ae44524a8d20751c7422
Giulia Frascaria <giulia.frascaria@gmail.com>
2019-12-07 16:18:50 +0100

    readfile.c

---
+++ docs/readfile.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+
+
+int main(void)
+{
+	//while(1)
+	//{
+		int fd = open("f", O_RDONLY);
+		if (fd == -1)
+		{
+			printf("error open file\n");
+			exit(EXIT_FAILURE);
+		}
+
+		char * buf = malloc(10);
+		ssize_t readbytes = read(fd, buf, 1);
+		close(fd);
+		free(buf);
+
+	//}
+
+
+	exit(EXIT_SUCCESS);
+}
