2f402ce7cab61a16f4d16f10bf18a8e2fd14d0d4
giuliafrascaria <giulia.frascaria@gmail.com>
2020-01-05 20:36:58 +0000

    I think I recompiled nicely

--- 415/src/newmake.sh
+++ 415/src/newmake.sh
@@ -27,7 +27,7 @@ gcc "$USER".c ../common/bpf_load.c ~/thesis/libbpf/src/libbpf.a -I/home/giogge/t
 
 }
 
-targets=( tracex1 sampleip cpustat attach_read)
+targets=( tracex1 cpustat)
 
 for t in "${targets[@]}" ; do
 	echo "making ...$t"
